f3554fa803d95e945e5a9be20e5d19d9ea913824
KiiiKi <45022052+KiiiKi@users.noreply.github.com>
2018-12-21 08:44:14 +0800

    用豆瓣api来添加数据

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -19,5 +19,29 @@ $(function(){
       }
     })
   })
+
+  $('#douban').blur(function(e){
+    var douban = $(this)
+    var id = douban.val()
+    if(id){
+      $.ajax({
+        url: 'https://api.douban.com/v2/movie/subject/' + id,
+        cache: true,
+        type: 'get',
+        dataType: 'jsonp',
+        crossDomain: true,
+        jsonp: 'callback',
+        success: function(data){
+          $('#inputTitle').val(data.title),//data来自豆瓣的数据返回
+          $('#inputDirector').val(data.directors[0].name),
+          $('#inputCountry').val(data.countries[0]),
+          $('#inputPoster').val(data.images.large),
+          $('#inputYear').val(data.year),
+          $('#inputSummary').val(data.summary)    
+        }
+      })
+    }
+  })
+
 })
 
